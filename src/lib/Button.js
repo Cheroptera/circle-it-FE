c9d587f1deecaed84d2a1ea24d41b33daa7ccbf1
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-05-30 15:25:08 +0200

    added button component

---
+++ src/lib/Button.js
@@ -0,0 +1,23 @@
+import styled, { css } from 'styled-components';
+
+export const Button = styled.button`
+border-radius: 16px;
+border: none;
+background-color: #A53860;
+color: white;
+font-size: 16px;
+font-weight: 700;
+padding: 12px 32px;
+margin: 10px;
+box-sizing: border-box;
+align-self: center;
+box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.5);
+
+
+${(props) => props.white && css`
+border: 2px #A53860 solid;
+background-color: white;
+color: black;
+padding: 6px 28px;
+`
+}`
