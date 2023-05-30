cb0a22dc9332fbb8b4075ddc5cff9180b6af9216
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-05-30 15:38:11 +0200

    added new url

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://project-authorization-hxor6euk4a-ew.a.run.app/'
+const BASE_URL = 'https://circle-it-be-3qae2ggbxq-no.a.run.app'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
