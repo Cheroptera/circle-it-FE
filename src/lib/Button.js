0f806058d559c11361cfa63662d6f7d9e5abb309
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-05-31 15:21:19 +0200

    Added onClick events to Button component

--- src/lib/Button.js
+++ src/lib/Button.js
@@ -1,11 +1,13 @@
 import styled, { css } from 'styled-components';
+import React from 'react';
 
-export const Button = styled.button`
+export const StyledButton = styled.button`
 border-radius: 16px;
 border: none;
 background-color: #A53860;
 color: white;
 font-size: 16px;
+
 font-weight: 700;
 padding: 12px 32px;
 margin: 10px;
@@ -21,3 +23,9 @@ color: black;
 padding: 6px 28px;
 `
 }`
+
+export const Button = ({ buttonText, handleClick }) => {
+  return (
+    <Button type="submit" onClick={handleClick}>{buttonText}</Button>
+  )
+}
