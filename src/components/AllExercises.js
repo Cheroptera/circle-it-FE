61b7dab6907f67feea0dabc57b76f8c28c8e1375
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-02 13:49:21 +0200

    Added catch error to AllExercises

--- src/components/AllExercises.js
+++ src/components/AllExercises.js
@@ -1,28 +1,47 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components/macro'
 import { API_URL } from 'utils/urls'
+import { useNavigate } from 'react-router-dom'
+// import { Details } from './components/details'
 
 export const AllExercises = () => {
   const [exerciseList, setExerciseList] = useState([])
+  // const [selectedExercise, setSelectedExercise] = useState()
+  const navigate = useNavigate()
 
   useEffect(() => {
     fetch(API_URL('exercises'))
       .then((res) => res.json())
       .then((json) => {
         setExerciseList(json);
-      });
-  }, []);
+      })
+      .catch((error) => {
+        console.error(error)
+        navigate('/404')
+      })
+  }, [navigate])
 
   return (
     <StyledList>
       <p>This is a list of all exercises</p>
       <ul>
         {exerciseList && exerciseList.map((singleExercise) => (
-          <li key={singleExercise.name}>
+          <p key={singleExercise.name}>
+            {/* <Link to={`/exercises/${singleExercise.name}`} onClick={() => navigate}> */}
             {singleExercise.name}
-          </li>
+            {/* </Link> */}
+          </p>
         ))}
       </ul>
+
+      {/* {selectedExercise && (
+        <Details
+          name={selectedExercise.name}
+          musclegroup={selectedExercise.musclegroup}
+          equipment={selectedExercise.equipment}
+          type={selectedExercise.type}
+          img={selectedExercise.img} />
+      )} */}
     </StyledList>
   )
 }
@@ -31,4 +50,5 @@ const StyledList = styled.div`
 display: flex; 
 flex-direction:column; 
 border: 2px solid green;
+list-style-type:none;
 `
