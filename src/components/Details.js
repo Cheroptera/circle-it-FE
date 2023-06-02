ff53b920347f885956af18d8d6685c36fe959810
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-02 13:46:42 +0200

    created Details component

---
+++ src/components/Details.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Details = () => {
+  return (
+    <div>
+      <h1>This is a details page where you can read more about a specific exercise!</h1>
+    </div>
+  );
+};
