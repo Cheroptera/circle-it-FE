2b82e0faa146fdb101a13e3991656b2ebd1d5997
Vio Szabo <vio@vioszabo.se>
2023-06-06 18:55:57 +0200

    Add subheadings to header

--- src/lib/Header.js
+++ src/lib/Header.js
@@ -2,28 +2,78 @@ import React from 'react'
 import styled from 'styled-components/macro'
 
 const StyledHeader = styled.div`
-display:flex;
-height: 250px;
-border-radius: 0px 0px 17rem 17rem;
-background-color: #61C9A8;
+  display: flex;
+  flex-direction: column;
+  height: 250px;
+  border-radius: 0px 0px 17rem 17rem;
+  background-color: #61c9a8;
 `
 
 const StyledHeaderTitle = styled.h2`
-align-self:center;
-color: white; 
-text-align: center;
-margin: 80px 30px 80px 30px;
+  font-family: 'Poppins', 'Sans-Serif';
+  align-self: center;
+  color: white;
+  text-align: center;
+  margin: 80px 30px 80px 30px;
 
-@media (min-width:1024px) {
-font-size: 48px;
-margin: 0 20%;
-}
+  @media (min-width: 1024px) {
+    font-size: 48px;
+    margin: 0 20%;
+  }
 `
 
-export const Header = ({ headerTitle, className }) => {
+const StyledheaderBackText = styled.p`
+  font-family: 'Poppins', 'Sans-Serif';
+  color: white;
+  font-size: 10px;
+  margin-right: 0;
+  margin-top: 0;
+`
+
+const StyledSubHeading = styled.h3`
+  font-family: 'Poppins', 'Sans-Serif';
+  align-self: center;
+  color: white;
+  text-align: center;
+  margin-top: 0;
+`
+
+const StyledCurrentRoundText = styled.h3`
+  font-family: 'Poppins', 'Sans-Serif';
+  align-self: center;
+  color: white;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 0;
+`
+
+export const Header = ({
+  headerTitle,
+  className,
+  headerBackText,
+  currentRoundText,
+  currentRepText,
+  headerSubheading,
+  headerNextUp
+}) => {
   return (
     <StyledHeader className={className}>
+      {headerBackText && (
+        <StyledheaderBackText>{headerBackText}</StyledheaderBackText>
+      )}
+      {currentRoundText && (
+        <StyledCurrentRoundText>{currentRoundText}</StyledCurrentRoundText>
+      )}
+      {currentRepText && (
+        <StyledCurrentRoundText>{currentRepText}</StyledCurrentRoundText>
+      )}
       {headerTitle && <StyledHeaderTitle>{headerTitle}</StyledHeaderTitle>}
+      {headerSubheading && (
+        <StyledSubHeading>{headerSubheading}</StyledSubHeading>
+      )}
+      {headerNextUp && (
+        <StyledSubHeading>Next up: {headerNextUp}</StyledSubHeading>
+      )}
     </StyledHeader>
   )
 }
