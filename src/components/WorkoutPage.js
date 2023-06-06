2a24a9e2a2fc29587c05a1d716c35244c9f804c5
Vio Szabo <vio@vioszabo.se>
2023-06-06 18:55:30 +0200

    WorkoutPage basic styling

--- src/components/WorkoutPage.js
+++ src/components/WorkoutPage.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+/* eslint-disable react/jsx-closing-bracket-location */
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import styled from 'styled-components/macro'
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import { Header } from '../lib/Header'
 
 export const WorkoutPage = () => {
   const randomList = useSelector((store) => store.exercises.list)
-  const workTime = useSelector((store) => store.timer.workTime);
+  const workTime = useSelector((store) => store.timer.workTime)
   const restTime = useSelector((store) => store.timer.restTime)
-  const repetitions = useSelector((store) => store.timer.repetitions);
+  const repetitions = useSelector((store) => store.timer.repetitions)
   const rounds = useSelector((store) => store.timer.rounds)
-  const roundsRestTime = useSelector((store) => store.timer.roundsRestTime);
+  const roundsRestTime = useSelector((store) => store.timer.roundsRestTime)
 
   const [isWorkTime, setIsWorkTime] = useState(true)
   const [isRestTime, setIsRestTime] = useState(false)
@@ -26,79 +28,122 @@ export const WorkoutPage = () => {
 
   const getNextExercise = () => {
     if (randomList.length > currentRepetition) {
-      return randomList[currentRepetition];
+      return randomList[currentRepetition]
     }
-    return null;
-  };
+    return null
+  }
 
   const handleTimerComplete = () => {
     if (isWorkTime) {
-      setIsWorkTime(false);
-      setIsRestTime(true);
+      setIsWorkTime(false)
+      setIsRestTime(true)
     } else if (isRestTime) {
-      setIsWorkTime(true);
-      setIsRestTime(false);
-      setCurrentRepetition((prevRepetition) => prevRepetition + 1);
+      setIsWorkTime(true)
+      setIsRestTime(false)
+      setCurrentRepetition((prevRepetition) => prevRepetition + 1)
     } else if (isRoundRest) {
-      setIsRoundRest(false);
-      setIsWorkTime(true);
-      setCurrentRepetition(1);
-      setCurrentRound((prevRound) => prevRound + 1);
+      setIsRoundRest(false)
+      setIsWorkTime(true)
+      setCurrentRepetition(1)
+      setCurrentRound((prevRound) => prevRound + 1)
     }
     if (currentRepetition === randomList.length) {
-      setIsRestTime(false);
-      setIsWorkTime(false);
-      setIsRoundRest(true);
+      setIsRestTime(false)
+      setIsWorkTime(false)
+      setIsRoundRest(true)
     }
-  };
+  }
   return (
-    <div>
-      <Header headerTitle="Workout page" />
-      <p>Current round: {currentRound} / {rounds}</p>
-      <p>Current rep: {currentRepetition} / {repetitions}</p>
+    <>
       {isWorkTime && (
-        <>
-          <h2>{getCurrentExercise().name}</h2>
-          <img src={getCurrentExercise().image} alt="Exercise" />
-          <CountdownCircleTimer
-            key={`work-${currentRepetition}-${currentRound}`}
-            isPlaying
-            duration={workTime}
-            colors={['#A53860']}
-            onComplete={handleTimerComplete}>
-            {({ remainingTime }) => remainingTime}
-          </CountdownCircleTimer>
-        </>
+        <Header
+          headerTitle={getCurrentExercise().name}
+          headerBackText="< Todays workout"
+          currentRoundText={`Round: ${currentRound} / ${rounds}`}
+          currentRepText={`Rep: ${currentRepetition} / ${repetitions}`}
+        />
       )}
-
       {isRestTime && (
-        <>
-          <h2>{getNextExercise().name}</h2>
-          <img src={getNextExercise().image} alt="Exercise" />
-          <CountdownCircleTimer
-            key={`rest-${currentRepetition}-${currentRound}`}
-            isPlaying
-            duration={restTime}
-            colors={['#3DA5D9']}
-            onComplete={handleTimerComplete}>
-            {({ remainingTime }) => remainingTime}
-          </CountdownCircleTimer>
-        </>
+        <Header
+          headerTitle="Rest"
+          headerNextUp={getNextExercise().name}
+          headerBackText="< Todays workout"
+          currentRoundText={`Round: ${currentRound} / ${rounds}`}
+          currentRepText={`Rep: ${currentRepetition} / ${repetitions}`}
+        />
       )}
-
       {isRoundRest && (
-        <>
-          <h2>Rest</h2>
-          <CountdownCircleTimer
-            key={`round-rest-${currentRound}`}
-            isPlaying
-            duration={roundsRestTime}
-            colors={['#9AFFDF']}
-            onComplete={handleTimerComplete}>
-            {({ remainingTime }) => remainingTime}
-          </CountdownCircleTimer>
-        </>
+        <Header
+          headerTitle="Rest"
+          headerBackText="< Todays workout"
+          currentRoundText={`Round: ${currentRound} / ${rounds}`}
+          currentRepText={`Rep: ${currentRepetition} / ${repetitions}`}
+        />
       )}
-    </div>
-  );
-};
+      <Main>
+        <WorkoutWrapper>
+          {isWorkTime && (
+            <>
+              <Img src={getCurrentExercise().img} alt="Exercise" />
+              <CountdownCircleTimer
+                key={`work-${currentRepetition}-${currentRound}`}
+                isPlaying
+                duration={workTime}
+                colors={['#A53860']}
+                onComplete={handleTimerComplete}>
+                {({ remainingTime }) => remainingTime}
+              </CountdownCircleTimer>
+            </>
+          )}
+
+          {isRestTime && (
+            <>
+              <Img src={getNextExercise().img} alt="Exercise" />
+              <CountdownCircleTimer
+                key={`rest-${currentRepetition}-${currentRound}`}
+                isPlaying
+                duration={restTime}
+                colors={['#3DA5D9']}
+                onComplete={handleTimerComplete}>
+                {({ remainingTime }) => remainingTime}
+              </CountdownCircleTimer>
+            </>
+          )}
+
+          {isRoundRest && (
+            <CountdownCircleTimer
+              key={`round-rest-${currentRound}`}
+              isPlaying
+              duration={roundsRestTime}
+              colors={['#9AFFDF']}
+              onComplete={handleTimerComplete}>
+              {({ remainingTime }) => remainingTime}
+            </CountdownCircleTimer>
+          )}
+        </WorkoutWrapper>
+      </Main>
+    </>
+  )
+}
+
+const Img = styled.img`
+  width: 200px;
+  padding: 10px;
+`
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const WorkoutWrapper = styled.div`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+  align-self: center;
+
+  @media (min-width: 1024px) {
+    justify-content: center;
+  }
+`
