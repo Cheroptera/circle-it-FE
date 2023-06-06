0d9ee30a13403b4c4deee580738d331eedcbb89e
Vio Szabo <vio@vioszabo.se>
2023-06-06 18:55:39 +0200

    SetTimer basic styling

--- src/components/SetTimer.js
+++ src/components/SetTimer.js
@@ -9,6 +9,9 @@ import {
   setRoundsRestTime,
   setIsRunning
 } from 'reducers/timer'
+import { Header } from 'lib/Header'
+import { StartButton } from 'lib/StartButton'
+import styled from 'styled-components/macro'
 
 //* This is where the user sets the timer
 export const SetTimer = () => {
@@ -24,60 +27,96 @@ export const SetTimer = () => {
   }
 
   return (
-    <div>
-      <label htmlFor="workout-time">
-        Workout Time:
-        <input
-          id="workout-time"
-          type="text"
-          value={workTime}
-          onChange={(e) => {
-            const inputValue = e.target.value
-            dispatch(setWorkTime(inputValue))
-          }}
-        />
-      </label>
-      <label htmlFor="rest-time">
-        Rest Time:
-        <input
-          id="rest-time"
-          type="text"
-          value={restTime}
-          onChange={(e) => {
-            const inputValue = e.target.value
-            dispatch(setRestTime(inputValue))
-          }}
-        />
-      </label>
-      <label htmlFor="repetitions">
-        Rounds:
-        <input
-          id="repetitions"
-          type="text"
-          value={rounds}
-          onChange={(e) => {
-            const inputValue = e.target.value
-            dispatch(setRounds(inputValue))
-          }}
-        />
-      </label>
-      <label htmlFor="round-rest-time">
-        Rest between rounds:
-        <input
-          id="rounds-rest-time"
-          type="text"
-          value={roundsRestTime}
-          onChange={(e) => {
-            const inputValue = e.target.value
-            dispatch(setRoundsRestTime(inputValue))
-          }}
-        />
-      </label>
-      <Link to="/workout">
-        <button type="button" onClick={handleStartWorkout}>
-          Lets go!
-        </button>
-      </Link>
-    </div>
+    <Main>
+      <Header headerTitle="What do you want to do today?" />
+      <FormWrapper>
+        <h3>Set your curcuit</h3>
+        <label htmlFor="rounds">
+          <Input
+            id="rounds"
+            type="text"
+            value={rounds}
+            onChange={(e) => {
+              const inputValue = e.target.value
+              dispatch(setRounds(inputValue))
+            }}
+          />{' '}
+          Rounds
+        </label>
+        <label htmlFor="workout-time">
+          <Input
+            id="workout-time"
+            type="text"
+            value={workTime}
+            onChange={(e) => {
+              const inputValue = e.target.value
+              dispatch(setWorkTime(inputValue))
+            }}
+          />{' '}
+          Work seconds
+        </label>
+        <label htmlFor="rest-time">
+          <Input
+            id="rest-time"
+            type="text"
+            value={restTime}
+            onChange={(e) => {
+              const inputValue = e.target.value
+              dispatch(setRestTime(inputValue))
+            }}
+          />{' '}
+          Rest seconds
+        </label>
+
+        <label htmlFor="round-rest-time">
+          <Input
+            id="rounds-rest-time"
+            type="text"
+            value={roundsRestTime}
+            onChange={(e) => {
+              const inputValue = e.target.value
+              dispatch(setRoundsRestTime(inputValue))
+            }}
+          />{' '}
+          Rest between rounds
+        </label>
+        <Link to="/workout">
+          <StartButton
+            type="button"
+            buttonText="Lets go!"
+            handleClickEvent={handleStartWorkout}
+          />
+        </Link>
+      </FormWrapper>
+    </Main>
   )
 }
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const FormWrapper = styled.div`
+  width: 242px;
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+
+  @media (min-width: 1024px) {
+    justify-content: center;
+  }
+`
+
+const Input = styled.input`
+width: 55px;
+height: 40px;
+background-color: #F0F0F0;
+margin: 10px;
+border-radius: 8px;
+text-align: center;
+
+:focus {
+  outline: none;
+  border: 3px solid #61C9A8;
+`
