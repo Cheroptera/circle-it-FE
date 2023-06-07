fdb9a6f8457daf6e681ef75443526dd3333a6bc2
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-07 11:58:33 +0200

    added globalstyle

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,12 +4,14 @@ const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    background: teal;
     font-family: 'Poppins', sans-serif;
   }
 
-  h2 {
-    font-weight: 600;
+  h1,
+  h2,
+  h3
+   {
+    font-weight: 700;
   }
 `
 
