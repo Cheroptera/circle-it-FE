6fb735ca4de71959c9d39cc346ccb9413e68f0a1
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-07 15:06:26 +0200

    styled Not Found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import Lottie from 'lottie-react'
+import notFound from '../lotties/notFound'
 
 const StyledNF = styled.div`
-border: 2px solid purple;
+display: flex;
+flex-direction: column;
+align-items: center;
+margin: 20px;
+
 `
 //* This is a page that is shown is what the user is looking for is not found
 export const NotFound = () => {
   return (
     <StyledNF>
+      <p>Ooops, something went wrong...</p>
+      <Lottie style={{ width: '200px', margin: 'auto' }} animationData={notFound} loop />
       <p>
         <Link to="/">Go to startpage</Link>
       </p>
-      <p>Sorry, nothing here...</p>
     </StyledNF>
   )
 }
