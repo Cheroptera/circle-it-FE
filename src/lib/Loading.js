96b492c0d0766dacb8363f1b24c0de16d79ab9c8
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-07 15:07:51 +0200

    started Loading component

---
+++ src/lib/Loading.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import Lottie from 'lottie-react'
+import Runner from '../lotties/runner'
+
+export const Loading = () => <Lottie style={{ width: '200px', margin: 'auto' }} animationData={Runner} loop />
