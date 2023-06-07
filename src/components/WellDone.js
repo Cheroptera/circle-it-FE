f97a3bc71d495f82f38711778feeb40d35a25ea9
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-07 15:07:22 +0200

    styled well done page

--- src/components/WellDone.js
+++ src/components/WellDone.js
@@ -1,1 +1,33 @@
+import React from 'react'
+import { Header } from 'lib/Header'
+import Lottie from 'lottie-react'
+import styled from 'styled-components/macro'
+import confetti from '../lotties/confetti.json'
+
+export const WellDone = () => {
+  return (
+    <>
+      <Header
+        headerTitle="Well done!" />
+      <StyledWellDoneContainer>
+        <H2>You&apos;re frickin&apos; awesome! </H2>
+      </StyledWellDoneContainer>
+      <Lottie style={{ width: '90%', height: '90%', position: 'absolute', top: '50px' }} animationData={confetti} loop />
+
+    </>
+  )
+}
+
+const StyledWellDoneContainer = styled.div`
+display: flex; 
+justify-content: center; 
+font-size: 32px; 
+text-align: center;
+margin: 5%;
+`
+
+const H2 = styled.h2`
+margin: unset;
+`
+
 //* This is a page that the user will see after finishing a workout
