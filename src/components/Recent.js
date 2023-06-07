c89638ea797d6156409a2ec01507710aadf108bf
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-07 15:55:09 +0200

    trying to fetch recent workouts

--- src/components/Recent.js
+++ src/components/Recent.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { API_URL } from 'utils/urls';
 
-//* This is where the most recent workouts is showing for the logged in user
 export const Recent = () => {
+  const [recentWorkouts, setRecentWorkouts] = useState([]);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken'); // Get the access token from storage
+
+    const fetchRecentWorkouts = async () => {
+      try {
+        const response = await fetch(API_URL('/exercises/recent'), {
+          headers: {
+            Authorization: accessToken
+          }
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setRecentWorkouts(data.response);
+        } else {
+          console.error('Failed to fetch recent workouts');
+        }
+      } catch (error) {
+        console.error('Error fetching recent workouts:', error);
+      }
+    };
+
+    if (accessToken) {
+      fetchRecentWorkouts();
+    }
+  }, []);
+
   return (
-    <p>Your recent workouts</p>
-  )
-}
+    <div>
+      <h2>Your Recent Workouts</h2>
+      {recentWorkouts.length > 0 ? (
+        <ul>
+          {recentWorkouts.map((workout) => (
+            <li key={workout.timestamp}>{/* Render workout data here */}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No recent workouts</p>
+      )}
+    </div>
+  );
+};
