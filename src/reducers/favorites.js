a374b750ad96e531768dac0a3acc26eb2d3da597
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-07 15:55:26 +0200

    added GoBackBtn to Header

--- src/reducers/favorites.js
+++ src/reducers/favorites.js
@@ -1,32 +1,32 @@
-// import { createSlice } from '@reduxjs/toolkit'
-// import { StrictMode } from 'react';
+import { createSlice } from '@reduxjs/toolkit'
+import { StrictMode } from 'react';
 
-// const favorites = createSlice({
-//   name: 'favorites',
-//   initialState: {
-//     items: [],
-//     error: null
-//   },
-//   reducers: {
-//     setError: (store, action) => {
-//       store.error = action.payload
-//     },
-//     setFavorites: (store, action) => {
-//       store.items = action.payload
-//     },
-//     toggleFavorite: (store, action) => {
-//       const exerciseId = action.payload;
-//       const index = store.indexOf(exerciseId);
-//       if (index !== -1) {
-//         // If the exerciseId is already in favorites, remove it
-//         StrictMode.splice(index, 1);
-//       } else {
-//         // If the exerciseId is not in favorites, add it
-//         store.push(exerciseId);
-//       }
-//     }
-//   }
-// })
+const favorites = createSlice({
+  name: 'favorites',
+  initialState: {
+    items: [],
+    error: null
+  },
+  reducers: {
+    setError: (store, action) => {
+      store.error = action.payload
+    },
+    setFavorites: (store, action) => {
+      store.items = action.payload
+    },
+    toggleFavorite: (store, action) => {
+      const exerciseId = action.payload;
+      const index = store.indexOf(exerciseId);
+      if (index !== -1) {
+        // If the exerciseId is already in favorites, remove it
+        StrictMode.splice(index, 1);
+      } else {
+        // If the exerciseId is not in favorites, add it
+        store.push(exerciseId);
+      }
+    }
+  }
+})
 
-// export const { toggleFavorite } = favorites.actions;
-// export default favorites
+export const { toggleFavorite } = favorites.actions;
+export default favorites
