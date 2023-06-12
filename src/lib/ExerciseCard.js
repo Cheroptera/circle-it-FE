99e30f3396438b2f05529e18cb6096990ca4a0be
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-12 16:13:46 +0200

    added css

--- src/lib/ExerciseCard.js
+++ src/lib/ExerciseCard.js
@@ -1,13 +1,23 @@
-import styled from 'styled-components/macro'
+import styled, { css } from 'styled-components/macro';
 
 export const ExerciseCard = styled.button`
-background: #9AFFDF;
-border: none;
-border-radius: 16px;
-box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.5);
-width: 250px;
-padding: 16px;
-display: flex;
-margin: 16px;
-justify-content: center;
-`
+  background: #9affdf;
+  border: none;
+  border-radius: 16px;
+  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.5);
+  width: 250px;
+  padding: 16px;
+  display: flex;
+  margin: 16px;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover{
+    background: #61C9A8;
+  }
+
+  ${({ isSelected }) => isSelected
+  && css`
+      background: #61C9A8;
+    `}
+`;
