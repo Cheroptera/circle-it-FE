67926a8bd2fa5189e388ba3d05c043e7703c5048
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-12 17:18:37 +0200

    deleted CustomWorkout

--- src/components/CustomWorkout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Header } from 'lib/Header'
-
-// * This is the page where the user selects what exercises to use
-export const CustomWorkout = () => {
-  return (
-    <Header headerTitle="I am a custom workout!" />
-  )
-}
