9cde865630a6fc5d6fc69f09e31c67674c1191f3
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-13 11:28:22 +0200

    imports and new reducers to combinereducers

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { user } from 'reducers/user'
 import { welcome } from 'reducers/welcome'
 import { timer } from 'reducers/timer'
 import { workouts } from 'reducers/workouts'
-import { custom } from 'reducers/customWorkout'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
@@ -16,7 +15,6 @@ import { Details } from 'components/Details'
 import { WorkoutPage } from 'components/WorkoutPage'
 import { NotFound } from 'components/NotFound'
 import { WellDone } from 'components/WellDone'
-import favorites from 'reducers/favorites'
 import { FilterData } from 'components/FilterData'
 import { AllExercises } from './components/AllExercises'
 import { LogIn } from './components/LogIn'
@@ -27,9 +25,7 @@ export const App = () => {
     user: user.reducer,
     welcome: welcome.reducer,
     timer: timer.reducer,
-    workouts: workouts.reducer,
-    favorites: favorites.reducer,
-    custom: custom.reducer
+    workouts: workouts.reducer
   })
   const store = configureStore({ reducer })
 
