924d66c0bf4e28727736cc4bda690fb3c9fb0c79
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-13 11:25:48 +0200

    added loading state

--- src/components/FilterData.js
+++ src/components/FilterData.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { setList } from 'reducers/workouts';
+import { setList } from 'reducers/workouts'
 import { API_URL } from 'utils/urls'
 import { Header } from 'lib/Header'
 
@@ -11,6 +11,7 @@ export const FilterData = () => {
   const navigate = useNavigate()
   const accessToken = useSelector((store) => store.user.accessToken)
 
+  const [loading, setLoading] = useState(true);
   const [filteredData, setFilteredData] = useState([])
   const [selectedMuscleGroups, setSelectedMuscleGroups] = useState([])
   const [selectedEquipment, setSelectedEquipment] = useState([])
@@ -24,7 +25,7 @@ export const FilterData = () => {
     mode: 'no-cors',
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
+      // 'Access-Control-Allow-Origin': '*',
       Authorization: accessToken,
       'X-RapidAPI-Key': process.env.REACT_APP_API_KEY
     }
@@ -38,33 +39,34 @@ export const FilterData = () => {
         impact: lowImpactOnly ? 'low' : ''
       });
       const response = await fetch(API_URL(`exercises/filter?${query}`), options)
-      const data = await response.json();
-      setFilteredData(data.response);
+      const data = await response.json()
+      setFilteredData(data.response)
+      setLoading(false)
     } catch (error) {
-      console.error('Error fetching filtered exercises:', error);
+      console.error('Error fetching filtered exercises:', error)
     }
   };
 
   const handleMuscleGroupChange = (event) => {
-    const checkedMuscleGroup = event.target.value;
+    const checkedMuscleGroup = event.target.value
     if (event.target.checked) {
-      setSelectedMuscleGroups((prevSelected) => [...prevSelected, checkedMuscleGroup]);
+      setSelectedMuscleGroups((prevSelected) => [...prevSelected, checkedMuscleGroup])
     } else {
-      setSelectedMuscleGroups((prevSelected) => prevSelected.filter((item) => item !== checkedMuscleGroup));
+      setSelectedMuscleGroups((prevSelected) => prevSelected.filter((item) => item !== checkedMuscleGroup))
     }
   };
 
   const handleEquipmentChange = (event) => {
-    const checkedEquipment = event.target.value;
+    const checkedEquipment = event.target.value
     if (event.target.checked) {
-      setSelectedEquipment((prevSelected) => [...prevSelected, checkedEquipment]);
+      setSelectedEquipment((prevSelected) => [...prevSelected, checkedEquipment])
     } else {
-      setSelectedEquipment((prevSelected) => prevSelected.filter((item) => item !== checkedEquipment));
+      setSelectedEquipment((prevSelected) => prevSelected.filter((item) => item !== checkedEquipment))
     }
   };
 
   const handleLowImpactToggle = (event) => {
-    setLowImpactOnly(event.target.checked);
+    setLowImpactOnly(event.target.checked)
   };
 
   const handleSetList = () => {
@@ -77,46 +79,52 @@ export const FilterData = () => {
   return (
     <>
       <Header headerTitle="Customize workout" />
-      {/* Render muscle group checkboxes */}
-      {musclegroups.map((singleMuscleGroup) => (
-        <div key={singleMuscleGroup}>
-          <label htmlFor={singleMuscleGroup}>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {/* Render muscle group checkboxes */}
+          {musclegroups.map((singleMuscleGroup) => (
+            <div key={singleMuscleGroup}>
+              <label htmlFor={singleMuscleGroup}>
+                <input
+                  type="checkbox"
+                  id={singleMuscleGroup}
+                  value={singleMuscleGroup}
+                  checked={selectedMuscleGroups.includes(singleMuscleGroup)}
+                  onChange={handleMuscleGroupChange} />
+                {singleMuscleGroup}
+              </label>
+            </div>
+          ))}
+
+          {/* Render equipment checkboxes */}
+          {equipment.map((singleEquipment) => (
+            <div key={singleEquipment}>
+              <label htmlFor={singleEquipment}>
+                <input
+                  type="checkbox"
+                  id={singleEquipment}
+                  value={singleEquipment}
+                  checked={selectedEquipment.includes(singleEquipment)}
+                  onChange={handleEquipmentChange} />
+                {singleEquipment}
+              </label>
+            </div>
+          ))}
+
+          {/* Render the low impact switch */}
+          <label htmlFor="lowImpact">
             <input
               type="checkbox"
-              id={singleMuscleGroup}
-              value={singleMuscleGroup}
-              checked={selectedMuscleGroups.includes(singleMuscleGroup)}
-              onChange={handleMuscleGroupChange} />
-            {singleMuscleGroup}
+              id="lowImpact"
+              checked={lowImpactOnly}
+              onChange={handleLowImpactToggle} />
+            Low Impact Only
           </label>
-        </div>
-      ))}
-
-      {/* Render equipment checkboxes */}
-      {equipment.map((singleEquipment) => (
-        <div key={singleEquipment}>
-          <label htmlFor={singleEquipment}>
-            <input
-              type="checkbox"
-              id={singleEquipment}
-              value={singleEquipment}
-              checked={selectedEquipment.includes(singleEquipment)}
-              onChange={handleEquipmentChange} />
-            {singleEquipment}
-          </label>
-        </div>
-      ))}
-
-      {/* Render the low impact switch */}
-      <label htmlFor="lowImpact">
-        <input
-          type="checkbox"
-          id="lowImpact"
-          checked={lowImpactOnly}
-          onChange={handleLowImpactToggle} />
-        Low Impact Only
-      </label>
-      <button type="button" onClick={handleSetList}>Randomize</button>
+          <button type="button" onClick={handleSetList}>Randomize</button>
+        </>
+      )}
     </>
   )
 }
