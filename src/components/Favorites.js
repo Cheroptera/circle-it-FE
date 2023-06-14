c1831470df1f1bddb432a880d7fb695ed6692765
theresBL <tbrannberglendt@gmail.com>
2023-06-14 16:25:35 +0200

    favorites fetch in progress

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,34 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+// import { useNavigate } from 'react-router-dom'
 import { API_URL } from 'utils/urls'
 import { Header } from 'lib/Header'
+import { LogOutButton } from 'lib/LogOutButton'
 
 // * This is a list of all the favorites of a logged in user
-export const Favorites = async () => {
-  const favorites = useSelector((store) => store.user.favorites)
+export const Favorites = () => {
+  // const navigate = useNavigate()
+  const accessToken = useSelector((store) => store.user.accessToken)
+  // const loggedInUserId = useSelector((store) => store.user.userId)
 
-  try {
-    const response = await fetch(API_URL('exercises/favorites'))
-    if (response.ok) {
-      await response.json()
-    } else {
-      throw new Error('Error fetching favorites')
+  // const [favoriteWorkouts, setFavoriteWorkouts] = useState([])
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: accessToken,
+        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+        'Access-Control-Allow-Origin': '*'
+      }
     }
-  } catch (error) {
-    console.log('Error fetching favorites:', error)
-  }
-
+    fetch(API_URL('workouts/favorites'), options)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+      })
+      .catch((error) => {
+        console.error('Failed', error)
+      })
+  })
   return (
     <>
       <Header />
       <div>
         <h2>My Favorites</h2>
-        <ul>
-          {favorites.map((favorite) => (
-            <li key={favorite.id}>{favorite.name}</li>
-          ))}
-        </ul>
       </div>
+      <LogOutButton />
     </>
   )
 }
@@ -78,3 +87,9 @@ export const Favorites = async () => {
 //     </>
 //   )
 // }
+// ,
+//       body: JSON.stringify({
+//         timestamp,
+//         exercises: finishedWorkout,
+//         loggedInUserId
+//       })
