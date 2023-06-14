87a513d021508b4f7f10b9fdbd6782f037e2771f
theresBL <tbrannberglendt@gmail.com>
2023-06-14 16:25:04 +0200

    changes url

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:8080'
-// 'https://circle-it-be-3qae2ggbxq-no.a.run.app'
+const BASE_URL = 'https://circle-it-be-3qae2ggbxq-no.a.run.app'
+// 'http://localhost:8080'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
 
