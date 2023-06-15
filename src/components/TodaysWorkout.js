59e4c2a42da0e0758c0de9c0e57040b3a069b349
theresBL <tbrannberglendt@gmail.com>
2023-06-15 13:10:34 +0200

    styling of new exercisecard in progress

--- src/components/TodaysWorkout.js
+++ src/components/TodaysWorkout.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 /* eslint-disable operator-linebreak */
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from 'react'
+import { React, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components/macro'
@@ -13,10 +13,20 @@ import { LogOutButton } from 'lib/LogOutButton'
 export const TodaysWorkout = () => {
   const workoutList = useSelector((store) => store.workouts.list)
   const navigate = useNavigate()
+  const [showDescriptionPopup, setShowDescriptionPopup] = useState(false)
+  const [selectedExercise, setSelectedExercise] = useState(null)
 
   const handleStart = () => {
     navigate('/set-timer')
   }
+  const handleDescriptionBtnClick = (exercise) => {
+    setSelectedExercise(exercise)
+    setShowDescriptionPopup(true)
+  }
+
+  const handleCloseDescriptionPopup = () => {
+    setShowDescriptionPopup(false);
+  };
 
   return (
     <>
@@ -30,7 +40,7 @@ export const TodaysWorkout = () => {
                   <ExerciseImg src={singleRandomExercise.img} alt="exerciseImg" />
                   <DescriptionDiv>
                     <h3>{singleRandomExercise.name}</h3>
-                    <p>{singleRandomExercise.description}</p>
+                    <DescriptionBtn type="button" onClick={handleDescriptionBtnClick}><p>{singleRandomExercise.description}</p><p>...</p></DescriptionBtn>
                   </DescriptionDiv>
                 </TodaysCard>
               </Li>
@@ -41,9 +51,38 @@ export const TodaysWorkout = () => {
         <StartButton buttonText="Set timer" handleClick={handleStart} />
       </ButtonWrapper>
       <LogOutButton />
+      {showDescriptionPopup && (
+        <DescriptionPopup
+          message={selectedExercise ? selectedExercise.description : ''}
+          handleClose={handleCloseDescriptionPopup} />
+      )}
     </>
   )
 }
+
+export const DescriptionBtn = styled.button`
+border: none;
+border-radius: 8px;
+box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.5);
+background-color: unset;
+font-size: 12px;
+width: 200px;
+cursor: pointer;
+text-align: inherit;
+overflow:hidden;
+text-overflow: ellipsis;
+word-break: break-all;
+white-space: nowrap;
+display: flex;
+flex-direction: column;
+
+&:hover {
+  opacity: 0.5;
+}
+@media (min-width: 700px){
+  width: 420px;
+}
+`
 
 export const StyledList = styled.div`
   display: flex;
@@ -73,12 +112,13 @@ border: solid #A53860 5px;
 display: flex;
 gap: 20px;
 width: 350px;
-height: fit-content;
+height: 110px;
 border-radius: 16px;
 box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.5);
 
 @media (min-width: 700px){
   width: 600px;
+  height: 130px;
 }
 `
 export const ExerciseImg = styled.img`
@@ -94,7 +134,9 @@ export const DescriptionDiv = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: flex-start;
-padding: 16px 10px 16px 0px;
+padding: 8px 10px 8px 0px;
+gap: 8px;
+width: 150px;
 
 h3{
   margin: unset;
@@ -104,4 +146,59 @@ p{
   margin: unset;
   margin-top: 6px;
 }
+`
+
+const DescriptionPopup = ({ message, handleClose }) => {
+  return (
+    <Overlay>
+      <AlertWindow>
+        <TopBorder>
+          <Xbutton
+            type="button"
+            onClick={handleClose}>
+            X
+          </Xbutton>
+        </TopBorder>
+        <MessageArea>
+          <p>{message}</p>
+        </MessageArea>
+      </AlertWindow>
+    </Overlay>
+  )
+}
+
+const Overlay = styled.section`
+width: 100vw;
+height: 100vh;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
+position: fixed;
+background-color: rgba(49,49,49,0.8);
+display: flex;
+align-items: center;
+justify-content: center;
+`
+
+const AlertWindow = styled.div`
+width: 300px;
+border: 3px #A53860 solid;
+border-radius: 10px 10px 5px 5px;
+position: absolute;
+background-color: white;
+`
+
+const TopBorder = styled.div`
+width: 100%;
+background-color: #A53860;
+display: flex;
+justify-content: flex-end;
+`
+const Xbutton = styled.button`
+margin: 3px;
+cursor:pointer;
+`
+const MessageArea = styled.div`
+padding: 0px 20px;
 `
